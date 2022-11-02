150e9e0d2c15ee0b65d510dbe1a20ea1f8982034
gelewaut <gelewaut@itba.edu.ar>
2022-11-02 10:56:22 -0300

    blockedProcessList

---
+++ Kernel/processManager/blockedProcessList.c
@@ -0,0 +1,25 @@
+#include <blockedProcessList.h>
+#include <memoryManager.h>
+#include <scheduler.h>
+
+blockedProcess * blockNewProcess(blockedProcess * first, process * toBlock) {
+    blockedProcess * newBlockedProcess = my_malloc(sizeof(blockedProcess));
+    if (newBlockedProcess == NULL)
+        return first;
+
+    toBlock->state = BLOCKED;
+    newBlockedProcess->head = toBlock;
+    newBlockedProcess->next = first;
+    return newBlockedProcess;
+}
+
+blockedProcess * unblockAllProcess (blockedProcess * first) {
+    blockedProcess * aux;
+    while(first != NULL) {
+        aux = first;
+        first = first->next;
+        aux->head->state = READY;
+        my_free(aux);
+    }
+    return first;
+}
